d49f5b504f7864fedc6c7b1a7a953cc5ddc57b7d
Oksana Kaledina <kaledinaoa@gmail.com>
2023-08-02 12:58:03 +0400

    scrappy 1st page

---
+++ classes.py
@@ -0,0 +1,13 @@
+from info_html import *
+
+# Define class Book
+class Book:
+    def __init__(self, title, price, score):
+        self.title = title
+        self.price = price
+        self.score = score
+
+    def get_title():
+        titles = card.find_all("span", {"class": BookInfo.title})
+        title = ' '.join([title.get_text().strip().replace('\n', '') for title in titles])
+        return title
---
+++ info_html.py
@@ -0,0 +1,11 @@
+
+class BookInfo:
+    title = [
+        "a-size-base-plus a-color-base a-text-normal",
+        "a-size-base-plus a-color-base a-text-bold a-text-normal"
+    ]
+    price = ["a-price-whole", "a-price-fraction"]
+    score = "a-icon-alt"
+
+class VideoInfo:
+    year = "a-size-base a-color-secondary a-text-normal"
